feb96d6267dafe9a56a2253abbb4eeb2287cf369
Fenton <haiyungg1016@gmail.com>
2024-08-29 00:08:14 +0800

    2024-08-29

---
+++ helloworld-app/src/com/itheima/hello/StudentInformation.java
@@ -0,0 +1,17 @@
+package com.itheima.hello;
+
+public class StudentInformation {
+    public static void main(String[] args) {
+        String name = "张三";
+        char sex = '男';
+        int age = 18;
+        float height = 1.78F;
+        boolean married = false;
+
+        System.out.println(name);
+        System.out.println(sex);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(married);
+    }
+}
